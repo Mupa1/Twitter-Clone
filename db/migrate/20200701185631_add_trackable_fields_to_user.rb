89dc6a3509634f246d5f688e3cab32a6f9c83b12
Mupa1 <mupasmail@gmail.com>
2020-07-01 21:41:55 +0200

    update users and tweets associations

---
+++ db/migrate/20200701185631_add_trackable_fields_to_user.rb
@@ -0,0 +1,4 @@
+class AddTrackableFieldsToUser < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_141858) do
+ActiveRecord::Schema.define(version: 2020_07_01_185631) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
